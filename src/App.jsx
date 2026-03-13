550409ebe7ce37cc88edb5a618060a922f70ead7
shubham200223feb <shubham2101861@gmail.com>
2026-03-13 15:40:32 +0530

    makeing pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Router } from 'react-router-dom'
+import { Route, Router, Routes } from 'react-router-dom'
 import Userpage from "./pages/Userpage.jsx"
 
 
@@ -7,9 +7,9 @@ import Userpage from "./pages/Userpage.jsx"
 const App = () => {
   return (
   
-    <Router>
+    <Routes>
       <Route path='/' element={Userpage}></Route>
-    </Router>
+    </Routes>
  
   )
 }
