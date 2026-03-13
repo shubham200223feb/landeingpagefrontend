373f63b01e1d4d82e9f8af19604d4890a08b725a
shubham200223feb <shubham2101861@gmail.com>
2026-03-13 16:57:50 +0530

    login form

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -34,10 +34,10 @@ export default function Account() {
                 <p className="text-slate-300 mt-1">Login to your account</p>
                 <form onSubmit={Submit} className="mt-8" onsubmit="return false">
                     <label htmlFor="email" className="block mb-1 font-medium text-slate-300">Email address</label>
-                    <input type="email" id="email" name="email" value={data.email} placeholder="Email" className="w-full p-2 mb-3 bg-slate-900 border border-slate-700 rounded-md focus:outline-none focus:ring-1 transition focus:ring-indigo-500 focus:border-indigo-500" />
+                    <input type="email" id="email" name="email" value={data.email} placeholder="Email" onChange={change} className="w-full p-2 mb-3 bg-slate-900 border border-slate-700 rounded-md focus:outline-none focus:ring-1 transition focus:ring-indigo-500 focus:border-indigo-500" />
             
                     <label htmlFor="password" className="block mb-1 font-medium text-slate-300">Password</label>
-                    <input type="password" id="password" name="password"  value={data.password} placeholder="Password" className="w-full p-2 mb-2 bg-slate-900 border border-slate-700 rounded-md focus:outline-none focus:ring-1 transition focus:ring-indigo-500 focus:border-indigo-500" />
+                    <input type="password" id="password" name="password"  value={data.password} onChange={change} placeholder="Password" className="w-full p-2 mb-2 bg-slate-900 border border-slate-700 rounded-md focus:outline-none focus:ring-1 transition focus:ring-indigo-500 focus:border-indigo-500" />
                     
                     <button type="submit" className="w-full mt-10 px-4 py-2.5 font-medium text-white bg-indigo-600 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500">{Loadeing?"plss wait..":"Sign in"}</button>
                 </form>
